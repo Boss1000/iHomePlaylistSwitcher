5dc4d787c9862e34848fb9e2f8859b6db69dc7ec
Boss1000 <boss1001@gmail.com>
2014-06-21 15:09:23 -0400

    Untested, but major update
    First time using the GitHub GUI program, which is really nice.
    Added and filled out config file.
    Wrote most of Python code

--- iHomePlaylistSwitcher.py
+++ iHomePlaylistSwitcher.py
@@ -3,9 +3,11 @@
 # By Ross Llewallyn
 
 # NOTE: Starting with just days of week
+# Ideally only load the relevant day/time/value
 
 import time
-import calendar
+from enum import Enum
+import xml.etree.ElementTree as ET # Use cElementTree?
 
 def iHomePlaylistSwitcher():
     """Checks the day of the week and changes the playlist in iTunes called "iHome" to whatever is pre-set for that day"""
@@ -25,41 +27,120 @@ def iHomePlaylistSwitcher():
             self.DayOfWeek   = DayOfWeek
             self.PlaylistStr = PlaylistStr
         
-        DayOfWeek = Monday
+        DayOfWeek = DayOfWeekType.Monday
         PlaylistStr = ""
-
-    def GetParamValue(inStr):
-        return inStr[(inStr.find("=") + 1):len(inStr)]
-
+    
+    def LineAComment(inStr):
+        return (inStr.lstrip()).startswith('#')
+    
+    def LineEmpty(inStr):
+        return (inStr.strip() is '')
+    
+    def GetParam(inStr):
+        return inStr[:inStr.find("=")]
+    
+    def GetValue(inStr):
+        return inStr[(inStr.find("=") + 1):]
+    
 # ---
     
-    iTunesLibraryLocation = ""
+    iTunesLibLoc = ""
     DayPlaylists = list()
+    DayPLStr = ""
+    iHomePLEle = None
+    DayPLEle   = None
+
+    #######################
+    # CONFIG FILE PARSING #
+    #######################
     
     # Load configuration file
-    f = open('iHomePlaylistSwitcher.cfg', 'r')
+    with open('iHomePlaylistSwitcher.cfg', 'r') as f1:
+        # Parse config file
+        for line in f1:
+            if   LineAComment(line):
+                continue
+            elif LineEmpty(line):
+                continue
+            elif '=' in line:
+                Param = GetParam(line)
+                Value = GetValue(line)
+                
+                if   "iTunesLibLoc" in Param:
+                    iTunesLibLoc = Value.rstrip()
+                elif "Alerts"       in Param:
+                    Value
+                elif "Monday"       in Param:
+                    DayPlaylists.append(DayPlaylist(DayOfWeekType.Monday,    Value))
+                elif "Tuesday"      in Param:
+                    DayPlaylists.append(DayPlaylist(DayOfWeekType.Tuesday,   Value))
+                elif "Wednesday"    in Param:
+                    DayPlaylists.append(DayPlaylist(DayOfWeekType.Wednesday, Value))
+                elif "Thursday"     in Param:
+                    DayPlaylists.append(DayPlaylist(DayOfWeekType.Thursday,  Value))
+                elif "Friday"       in Param:
+                    DayPlaylists.append(DayPlaylist(DayOfWeekType.Friday,    Value))
+                elif "Saturday"     in Param:
+                    DayPlaylists.append(DayPlaylist(DayOfWeekType.Saturday,  Value))
+                elif "Sunday"       in Param:
+                    DayPlaylists.append(DayPlaylist(DayOfWeekType.Sunday,    Value))
+    
+    # Config file closed
+    print(f1.closed)
 
-    for line in f:
-        if   "iTunesLibraryLocation" in line:
-            iTunesLibraryLocation = GetParamValue(line)
-        elif "Monday"    in line:
-            DayPlaylists.append(DayPlaylist(DayOfWeekType.Monday,    GetParamValue(line)))
-        elif "Tuesday"   in line:
-            DayPlaylists.append(DayPlaylist(DayOfWeekType.Tuesday,   GetParamValue(line)))
-        elif "Wednesday" in line:
-            DayPlaylists.append(DayPlaylist(DayOfWeekType.Wednesday, GetParamValue(line)))
-        elif "Thursday"  in line:
-            DayPlaylists.append(DayPlaylist(DayOfWeekType.Thursday,  GetParamValue(line)))
-        elif "Friday"    in line:
-            DayPlaylists.append(DayPlaylist(DayOfWeekType.Friday,    GetParamValue(line)))
-        elif "Saturday"  in line:
-            DayPlaylists.append(DayPlaylist(DayOfWeekType.Saturday,  GetParamValue(line)))
-        elif "Sunday"    in line:
-            DayPlaylists.append(DayPlaylist(DayOfWeekType.Sunday,    GetParamValue(line)))
-
-        
+    #########################
+    # DAY PLAYLIST MATCHING #
+    #########################
     
     # Get day of the week
     Weekday = time.gmtime(time.time()).tm_wday
+    
+    for day in DayPlaylists:
+        if Weekday is day.DayOfWeek:
+            DayPLStr = day.PLStr
+            break
+    
+    ##########################
+    # ITUNES LIBRARY PARSING #
+    ##########################
+    
+    if iTunesLibLoc != '':
+        with open(iTunesLibLoc, 'r+') as f2:
+            tree = ET.parse(f2) # Probably lengthy
+            root2 = tree.getroot()[0]
+            index = 0
+            childList = root2.getchildren()
+            for child in childList:
+                if child.text is "Playlists":
+                    plArray = root2[index+1] # Playlist <array> element is after "<key>Playlists</key>"
+                    break
+                index += 1
+            
+            for PL in plArray:
+                if PL[1].tag is "string" and PL[2].text is "Playlist ID":
+                    if PL[1].text is "iHome":
+                        iHomePLEle = PL
+                    if PL[1].text is DayPLStr:
+                        DayPLEle   = PL
+            
+            if iHomePLEle is None:
+                print("No iHome playlist found. Please create a blank one in iTunes and rerun.")
+            if DayPLEle   is None:
+                print("Today's playlist not found: ", DayPLStr)
+            
+            #####################################
+            # SAVE & REPLACE OLD IHOME PLAYLIST #
+            #####################################
+            
+            # TODO Save
+            
+            iHomeSongs = iHomePLEle.find("array")
+            iHomeSongs = DayPLEle.find("array")
+
+            tree.write(iTunesLibLoc + ".test")
+    
+    # Library file closed
+    print(f2.closed)
+    
     
     
